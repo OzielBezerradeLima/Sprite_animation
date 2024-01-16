5cfa92e22b49bebe4c131c4f095d6685a54b343f
Oziel Bezerra de Lima <obdl.snf23@uea.edu.br>
2024-01-16 08:48:44 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Spritesheets')
+
+#sprite_sheet_image = pygame.image.load('')
+
+BG = (50, 50, 50)
+
+run = True
+while run:
+
+    screen.fill(BG)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
